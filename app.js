7cfc8745657fcd3eeb53ed6f34dd602aa1ad8439
Parsa Ghasemi <parsa.ghasemi2012@gmail.com>
2023-02-24 11:45:51 +0330

    created chatroom public room
    public room is completed and i'm already working on private chatroom.

---
+++ app.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+const express = require('express');
+const { Server } = require('socket.io');
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server) 
+
+//StaticFiles
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000
+
+
+server.listen(PORT , () => {
+    console.log(`server is running on port ${PORT}`);
+})
+
+
+//WebSocket Creator
+users = {}
+io.on("connection" , (socket) => {
+
+//Listening
+    socket.on("login", (nickname) => {
+        console.log(`${nickname} connected`);
+        users[socket.id] = nickname;
+        io.sockets.emit("online", users)
+    })
+    socket.on("disconnect" , () => {
+        console.log(`user is disconnected`);
+        delete users[socket.id]
+        io.sockets.emit("online", users)
+    });
+    socket.on("chatProcces" , (data) => {
+         io.sockets.emit("chatProcces" , data)
+    });
+    socket.on("typing", (data) => {
+        socket.broadcast.emit("typing" ,data)})
+})
---
+++ public/js/chat.js
@@ -0,0 +1,88 @@
+const socket = io();
+
+//Query DOM
+const messageInput = document.getElementById("messageInput"),
+    chatForm = document.getElementById("chatForm"),
+    chatBox = document.getElementById("chat-box"),
+    feedback = document.getElementById("feedback"),
+    onlineUsers = document.getElementById("online-users-list");
+chatContainer = document.getElementById("chat-container")
+
+const nickname = localStorage.getItem("nickname");
+let socketId
+// Emit Events
+socket.emit("login", nickname);
+
+chatForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (messageInput.value) {
+        socket.emit("chatProcces", {
+            message: messageInput.value,
+            name: nickname,
+        });
+        messageInput.value = "";
+    }
+});
+
+messageInput.addEventListener("keypress", () => {
+    socket.emit("typing", {
+        name: nickname
+    });
+});
+
+// Listening
+
+socket.on("online", (users) => {
+    onlineUsers.innerHTML = ""
+    for (const socketId in users) {
+        onlineUsers.innerHTML += `
+        <li>
+            <button type="button" class="btn btn-light mx-2 p-2" data-toggle="modal" data-target="#pvChat" data-id=${socketId} data-client=${users[socketId]}>
+            ${users[socketId]}
+            <span class="badge badge-success"></span>
+            </button>
+        </li>
+        `;
+    }
+});
+
+socket.on("chatProcces", (data) => {
+    feedback.innerHTML = "";
+    chatBox.innerHTML += `
+                        <li class="alert alert-light">
+                            <span
+                                class="text-dark font-weight-normal"
+                                style="font-size: 13pt"
+                                >${data.name}</span
+                            >
+                            <span
+                                class="
+                                    text-muted
+                                    font-italic font-weight-light
+                                    m-2
+                                "
+                                style="font-size: 9pt"
+                                >ساعت 12:00</span
+                            >
+                            <p
+                                class="alert alert-info mt-2"
+                                style="font-family: persian01"
+                            >
+                            ${data.message}
+                            </p>
+                        </li>`;
+    chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight
+});
+
+socket.on("typing", (data) => {
+    feedback.innerHTML = `<p class="alert alert-warning w-25"><em>${data.name} در حال نوشتن است ... </em></p>`;
+});
+
+
+//JQuery
+$("#pvChat").on("show.bs.modal", function (e) {
+    var button = $(e.relatedTarget)
+    var user = button.data("client")
+    socketId = button.data("id")
+    
+})
